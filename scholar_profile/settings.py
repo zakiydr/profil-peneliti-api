5b16b9957a97a067deaed964c818181d96866575
Zakiyuddin Rahman <zakiyuddinr@gmail.com>
2024-11-01 10:21:58 +0700

    reconfigure deployment

--- scholar_profile/settings.py
+++ scholar_profile/settings.py
@@ -42,8 +42,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "goscholar"
+    'corsheaders',
+    "goscholar",
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [

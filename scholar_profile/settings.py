8030a50d9fa43792576c89ae8f5996de093115d9
Zakiyuddin Rahman <zakiyuddinr@gmail.com>
2025-03-08 11:20:08 +0700

    refactor: change proxy rotation middleware settings

--- scholar_profile/settings.py
+++ scholar_profile/settings.py
@@ -77,7 +77,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
-    # 'goscholar.middleware.ProxyMiddleware',
+    'goscholar.middleware.SwiftshadowProxyMiddleware',
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",

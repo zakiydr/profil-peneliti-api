8a770f3cd25dae46f8e20104bcb7e257faf17dce
Zakiyuddin Rahman <zakiyuddinr@gmail.com>
2025-03-24 15:54:18 +0700

    refactor: set debug false for vercel

--- scholar_profile/settings.py
+++ scholar_profile/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-qhk32!-5+oybvnkd92*dnz=zrc47bb0&jpvzmt=k6q%@_gawyfu7!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '*', 'zakiydr.pythonanywhere.com']
 

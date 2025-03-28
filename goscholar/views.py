7888eb781b9baa13f50f0eec0eda2e67ccf1b76e
Zakiyuddin Rahman <zakiyuddinr@gmail.com>
2025-03-28 21:14:11 +0700

    refactor: package and proxy update

--- goscholar/views.py
+++ goscholar/views.py
@@ -3,8 +3,7 @@ import requests
 from functools import wraps
 import time
 
-from scholarly import scholarly
-from scholarly import ProxyGenerator
+from scholarly import scholarly, ProxyGenerator
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status

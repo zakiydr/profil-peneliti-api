b9f99e0bd0acf233b457f3b1501dc166045ed273
Zakiyuddin Rahman <zakiyuddinr@gmail.com>
2025-03-29 11:34:09 +0700

    refactor: update cors implementation

--- scholar_profile/settings.py
+++ scholar_profile/settings.py
@@ -45,25 +45,15 @@ INSTALLED_APPS = [
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "https://zakiydr.pythonanywhere.com",
 ]
 
-CORS_ALLOW_METHODS = [
-    "GET",
-    "POST",
-    "PUT",
-    "PATCH",
-    "DELETE",
-    "OPTIONS"
-]
+CORS_ALLOW_METHODS = ["*"]
 
-CORS_ALLOW_HEADERS = [
-    "content-type",
-    "authorization",
-    "x-csrftoken",
-]
+CORS_ALLOW_HEADERS = ["*"]
 
 
 

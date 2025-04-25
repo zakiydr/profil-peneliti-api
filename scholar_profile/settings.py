c0d935b72fb2de3852c2556413adce7d6f163015
Zakiyuddin Rahman <zakiyuddinr@gmail.com>
2025-04-25 17:50:20 +0700

    refactor: save private information into .env

--- scholar_profile/settings.py
+++ scholar_profile/settings.py
@@ -14,7 +14,9 @@ import os
 from pathlib import Path
 from corsheaders.defaults import default_methods
 from corsheaders.defaults import default_headers
+from dotenv import load_dotenv
 
+load_dotenv('.env.dev')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,13 +26,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-qhk32!-5+oybvnkd92*dnz=zrc47bb0&jpvzmt=k6q%@_gawyfu7!"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = ['.vercel.app', '*', 'zakiydr.pythonanywhere.com']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
+CSRF_TRUSTED_ORIGINS = os.environ.get("DJANGO_CSRF_TRUSTED_ORIGINS", "").split(",")
 
 # Application definition
 
@@ -179,6 +182,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
